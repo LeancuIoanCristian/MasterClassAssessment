b8063192842dfc9d5495981c4c901dea9e680018
LeancuIoanCristian <71566273+LeancuIoanCristian@users.noreply.github.com>
2023-02-10 22:18:26 +0000

    Add files via upload

---
+++ CarAdditions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAdditions : ScriptableObject
+{
+    //additional equipment name
+    public string equipment_name;
+
+    //price multyplier
+    float multiplier;
+
+    //equipment price
+    public float equipment_price;
+}
